97201222e51f341b44cae9792b4cc69b17f9f957
Pablo Oliveira <pablo@kraftz.io>
2016-02-25 13:52:01 -0300

    refactor(index): moves the progress bar to utils module

--- util.js
+++ util.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const ProgressBar = require('progress');
+
 module.exports.getSizeOf = (value) => {
   if (typeof value === 'string') {
     return Buffer.byteLength(value, 'utf8');
@@ -22,4 +24,24 @@ module.exports.calcRate = (base, number, precision) => {
   }
 
   return ((number * 100) / base).toFixed(precision);
+};
+
+module.exports.progressBar = (template, total, length) => {
+  length = length || 20;
+  let bar;
+
+  if (process.env.verbose == 'true') {
+    let barTemplate = template;
+
+    let barOptions = {
+      complete: '=',
+      incomplete: ' ',
+      width: length,
+      total: total
+    };
+
+    bar = new ProgressBar(barTemplate, barOptions);
+  }
+
+  return bar;
 };
